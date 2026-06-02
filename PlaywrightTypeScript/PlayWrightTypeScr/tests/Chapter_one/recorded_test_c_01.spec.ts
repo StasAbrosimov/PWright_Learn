cc6d775f7adfaca03f4fb70b01c24da7b1ff30da
stas_abrosimov <s.abrosimov@program-ace.com>
2026-06-02 13:58:29 +0300

    refactor projects

---
+++ PlaywrightTypeScript/PlayWrightTypeScr/tests/Chapter_one/recorded_test_c_01.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://github.com/');
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByRole('textbox', { name: 'Username or email address' }).click();
+  await page.getByRole('textbox', { name: 'Username or email address' }).fill('testone');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('testonepass');
+  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+  await expect(page.getByRole('alert').first()).toBeVisible();
+});
