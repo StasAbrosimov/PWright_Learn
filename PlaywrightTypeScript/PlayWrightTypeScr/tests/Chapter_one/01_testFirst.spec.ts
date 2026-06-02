d0bb5fd2d5449d432e56698f6824381004b1ccdb
stas_abrosimov <s.abrosimov@program-ace.com>
2026-06-02 18:14:01 +0300

    record to cursor

--- PlaywrightTypeScript/PlayWrightTypeScr/tests/Chapter_one/01_testFirst.spec.ts
+++ PlaywrightTypeScript/PlayWrightTypeScr/tests/Chapter_one/01_testFirst.spec.ts
@@ -1,9 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test("Goggle search", async ({ page }) => {
+test("playwright search", async ({ page }) => {
     await page.goto('https://playwright.dev');
+
     await page.getByRole('button', { name: 'Search (Ctrl+K)' }).click();
+
     const searchInput = page.getByRole('searchbox', { name: 'Search' });
+
     await searchInput.click()
     await page.waitForTimeout(250);
     await searchInput.fill("page");
@@ -11,8 +14,34 @@ test("Goggle search", async ({ page }) => {
     await searchInput.click()
     await page.waitForTimeout(250);
     await searchInput.press("Enter");
+
     await page.getByRole('link', { name: 'BrowserContext' }).click();
     await page.waitForTimeout(250);
-    expect(page.getByRole('heading', { name: 'BrowserContext' })).toBeVisible();
 
+    expect(page.getByRole('heading', { name: 'BrowserContext' })).toBeVisible();
+    expect(page).toHaveTitle("BrowserContext | Playwright");
+});
+
+
+test("playwright search record to cursor", async ({ page }) => {
+    await page.goto('https://playwright.dev');
+
+    await page.getByRole('button', { name: 'Search (Ctrl+K)' }).click();
+
+    const searchInput = page.getByRole('searchbox', { name: 'Search' });
+
+    await searchInput.click()
+    await page.waitForTimeout(250);
+    await searchInput.fill("page");
+    await page.waitForTimeout(250);
+    await searchInput.click()
+    await page.waitForTimeout(250);
+    await searchInput.press("Enter");
+
+    await page.getByRole('link', { name: 'BrowserContext' }).click();
+    await page.waitForTimeout(250);
+
+    await expect(page.getByRole('heading', { name: 'BrowserContext' })).toBeVisible();
+    await expect(page).toHaveTitle("BrowserContext | Playwright");
+    await expect(page.getByRole('article')).toContainText('Removes cookies from context. Accepts optional filter.');
 });
